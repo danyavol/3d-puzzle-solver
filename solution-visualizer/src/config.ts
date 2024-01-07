ef93fde919fc06822fe5edf7b52ff25ec35bea5e
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2024-01-07 17:48:38 +0100

    Init Three.js

---
+++ solution-visualizer/src/config.ts
@@ -0,0 +1,53 @@
+import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+export function initThreeJs() {
+    // Init Scene
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xf5f5f5 );
+
+    // Init renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    // Init camera
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set( 8, 8, 8 );
+
+    // Init camera controls
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.minDistance = 3;
+    controls.maxDistance = 10;
+    controls.target = new THREE.Vector3(2, 2, 2);
+
+    // Add light to Scene
+    scene.add( new THREE.AmbientLight( 0x666666 ) );
+
+    const light = new THREE.DirectionalLight( 0xffffff, 3 );
+    light.position.set( 0, 500, 2000 );
+    scene.add( light ); 
+
+    // Temporary Axis Visualizer
+    // X - red, Y - green, Z - blue
+    scene.add( new THREE.AxesHelper( 10 ) );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    return { scene, animate }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+    
+    function animate() {
+        requestAnimationFrame( animate );
+    
+        renderer.render( scene, camera );
+    }    
+}
+
--- solution-visualizer/src/main.ts
+++ solution-visualizer/src/main.ts
@@ -1,3 +1,32 @@
-import elements from '../../data/elements.json';
+import "./styles/styles.scss";
 
-document.getElementById('app')!.innerText = JSON.stringify(elements);
+import * as THREE from 'three';
+import { ConvexGeometry } from 'three/addons/geometries/ConvexGeometry.js';
+import { initThreeJs } from './config';
+
+// Setup
+
+const { scene, animate } = initThreeJs();
+
+const meshMaterial = new THREE.MeshLambertMaterial( {
+    color: 0xff0000,
+    opacity: 0.5,
+    side: THREE.DoubleSide,
+    transparent: true
+} );
+
+const points = [];
+points.push( new THREE.Vector3( 0, 0, 0 ) );
+points.push( new THREE.Vector3( 1, 0, 1 ) );
+points.push( new THREE.Vector3( 0, 0, 2 ) );
+points.push( new THREE.Vector3( 0, 1, 0 ) );
+points.push( new THREE.Vector3( 1, 1, 1 ) );
+points.push( new THREE.Vector3( 0, 1, 2 ) );
+
+const meshGeometry = new ConvexGeometry( points );
+const triangle = new THREE.Mesh( meshGeometry, meshMaterial );
+
+scene.add( triangle );
+
+
+animate();
